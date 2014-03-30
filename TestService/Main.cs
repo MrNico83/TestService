3b8c8b373ae4e6cef880a1715bc3b77324ddeea6
MrNico83 <nlautridou@gmail.com>
2014-03-30 14:37:25 +0200

    q X

---
+++ TestService/Main.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace TestService
+{
+  /// <summary>
+  /// Main class.
+  /// </summary>
+  public class MainClass
+  {
+    /// <summary>
+    /// The entry point of the program, where the program control starts and ends.
+    /// </summary>
+    /// <param name='args'>The command-line arguments.</param>
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
